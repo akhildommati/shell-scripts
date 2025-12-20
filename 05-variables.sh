3dadc22271293ef82338eb4dbed8d5ae0a06ebd1
akhil <akhilprince567@gmail.com>
2025-12-20 01:21:48 -0500

    conversation added

--- 05-variables.sh
+++ 05-variables.sh
@@ -5,3 +5,7 @@ echo "Please enter your userrname:" # prompt for user input
 read -s USERNAME # here username is the variable to store input
 
 echo "username entered: $USERNAME"
+
+echo "Please enter your password:" # prompt for user input
+
+read -s PASSWORD # -s flag to hide input while typing
