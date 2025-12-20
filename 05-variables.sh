8f7afc640d1a38bad285d242fbe05a2db0721199
akhil <akhilprince567@gmail.com>
2025-12-20 01:19:53 -0500

    conversation added

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-echo "Please enter your userrname:"
-read USERNAME
+echo "Please enter your userrname:" # prompt for user input
+
+read USERNAME # here username is the variable to store input
+
+echo "username entered: $USERNAME"
