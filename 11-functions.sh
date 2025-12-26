466b3da976c3b39abea6e97139b64f6ab7940705
akhil <akhilprince567@gmail.com>
2025-12-26 14:53:06 -0500

    installing mysql

--- 11-functions.sh
+++ 11-functions.sh
@@ -2,7 +2,7 @@
 
 USERID=$(id -u)
 VALIDATE(){
-    if [$1 -ne 0]
+    if [ $1 -ne 0 ]
     then 
     echo "$2 Mysql.....FAILURE"
     exit 1
@@ -19,7 +19,7 @@ fi
 
 dnf list installed mysql
 
-if [$? -ne 0] # not installed
+if [ $? -ne 0 ] # not installed
 then
     dnf install mysql -y
     VALIDATE $? ""Installing Mysql
@@ -29,7 +29,7 @@ fi
 
 dnf list installed git
 
-if [$? -ne 0] # not installed
+if [ $? -ne 0 ] # not installed
 then
     dnf install git -y
     VALIDATE $? "Installing Git"
