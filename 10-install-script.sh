7dab394598b3d68a44e40721bd069267de72470e
akhil <akhilprince567@gmail.com>
2025-12-26 13:18:16 -0500

    installing mysql

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -1,28 +1,3 @@
 #!/bin/bash
 
-USERID=$(id -u)
-if [ $USERID -ne 0 ]
-then
-    echo "ERROR :: Please run this script as root or using sudo."
-    exit 1 #other than 0 means error
-fi
-
 dnf install mysql -y
-
-if [ $? -ne 0 ]
-then
-    echo "ERROR :: MySQL installation failed."
-    exit 1
-else
-    echo "MySQL installed successfully."
-fi
-
-dnf install git -y
-
-if [ $? -ne 0 ]
-then
-    echo "ERROR :: Git installation failed."
-    exit 1
-else
-    echo "Git installed successfully."
-fi
