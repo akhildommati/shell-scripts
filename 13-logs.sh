d792ac13f3e6069017efe770ccad2e8ec1ce3abe
akhil <akhilprince567@gmail.com>
2025-12-26 15:51:04 -0500

    installing mysql

---
+++ 13-logs.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +Y-%m-%d-%H-%M-%S )
+LOG_FILE_NAME="$LOGS_FOLDER/"$LOG_FILE"-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+    echo -e "$2 ......$R FAILURE"
+    exit 1
+    else
+    echo -e "$2 ......$G SUCCESS"
+    fi  
+}
+
+echo "Script started executed at: $TIMESTAMP" &>>$LOG_FILE_NAME
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR : you must have sudo access to execute this script."
+    exit 1
+fi  
+
+dnf list installed mysql  &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ] # not installed
+then
+    dnf install mysql -y  &>>$LOG_FILE_NAME
+    VALIDATE $? ""Installing Mysql
+else
+    echo -e "Mysql is already.....$Y installed"
+fi  
+
+dnf list installed git   &>>$LOG_FILE_NAME
+
+if [ $? -ne 0 ] # not installed
+then
+    dnf install git -y   &>>$LOG_FILE_NAME
+    VALIDATE $? "Installing Git"
+else
+    echo -e "Git is already....$Y installed"
+fi
+
+
