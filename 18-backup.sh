3a5b4213a84b2a5a3ca3a5bd80ddb49a37b80dc0
akhil <akhilprince567@gmail.com>
2025-12-29 00:01:35 -0500

    creating backup

---
+++ 18-backup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14}  # Default to 14 days if not provided
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +Y-%m-%d-%H-%M-%S )
+LOG_FILE_NAME="$LOGS_FOLDER/"$LOG_FILE"-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+    echo -e "$2 ......$R FAILURE $N"
+    exit 1
+    else
+    echo -e "$2 ......$G SUCCESS $N"
+    fi  
+}
+
+USAGE(){
+    echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIRECTORY> <DESTINATION_DIRECTORY> [DAYS_OLD]"
+}
+
+if [$# -lt 2 ]
+then
+    echo -e "$R ERROR: Please provide SOURCE and DESTINATION directories as arguments. $N"
+    exit 1
+fi
+
+echo "Script started executed at: $TIMESTAMP" &>>$LOG_FILE_NAME
